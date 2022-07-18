e86d5121dfeb40d95eabf59e48c1224a99bfa361
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-07-17 23:53:33 -0500

    Section 4 - Add ApplicationDbContext

---
+++ MagicVilla_CouponAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using MagicVilla_CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_CouponAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon()
+                {
+                    Id = 1,
+                    Name = "10OFF",
+                    Percent = 10,
+                    IsActive = true,
+                },
+                new Coupon()
+                {
+                    Id = 2,
+                    Name = "20OFF",
+                    Percent = 20,
+                    IsActive = true,
+                });
+        }
+    }
+}
