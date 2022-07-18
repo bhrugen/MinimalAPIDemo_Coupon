873421afa0d380d3acac035abf2e842a3fda5306
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-07-17 22:48:31 -0500

    Section 3 - Dependency Injection in Minimal API

--- MagicVilla_CouponAPI/Program.cs
+++ MagicVilla_CouponAPI/Program.cs
@@ -21,7 +21,8 @@ if (app.Environment.IsDevelopment())
 }
 
 
-app.MapGet("/api/coupon", () => {
+app.MapGet("/api/coupon", (ILogger<Program> _logger) => {
+    _logger.Log(LogLevel.Information, "Getting all Coupons");
     return Results.Ok(CouponStore.couponList);
 }).WithName("GetCoupons").Produces<IEnumerable<Coupon>>(200);
 
