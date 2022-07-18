52962a96aa13ce56c0b510fc6945d31d435e2b67
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-07-17 22:41:33 -0500

    Section 3 - Products and Accepts in Minimal API

--- MagicVilla_CouponAPI/Program.cs
+++ MagicVilla_CouponAPI/Program.cs
@@ -23,12 +23,12 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/api/coupon", () => {
     return Results.Ok(CouponStore.couponList);
-}).WithName("GetCoupons"); ;
+}).WithName("GetCoupons").Produces<IEnumerable<Coupon>>(200);
 
 
 app.MapGet("/api/coupon/{id:int}", (int id) => {
     return Results.Ok(CouponStore.couponList.FirstOrDefault(u=>u.Id==id));
-}).WithName("GetCoupon");
+}).WithName("GetCoupon").Produces<Coupon>(200);
 
 app.MapPost("/api/coupon", ([FromBody] Coupon coupon) => {
     if (coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name))
@@ -44,7 +44,7 @@ app.MapPost("/api/coupon", ([FromBody] Coupon coupon) => {
     CouponStore.couponList.Add(coupon);
     return Results.CreatedAtRoute("GetCoupon",new { id=coupon.Id }, coupon);
     //return Results.Created($"/api/coupon/{coupon.Id}",coupon);
-}).WithName("CreateCoupon"); ;
+}).WithName("CreateCoupon").Accepts<Coupon>("application/json").Produces<Coupon>(201).Produces(400);
 
 app.MapPut("/api/coupon", () => {
 
