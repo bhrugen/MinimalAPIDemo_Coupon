e20565e0cfde3d7bbf8592fd9f7007d58f295e38
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-07-18 15:00:03 -0500

    Section 4 - Repository for DbContext

---
+++ MagicVilla_CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,51 @@
+﻿using MagicVilla_CouponAPI.Data;
+using MagicVilla_CouponAPI.Models;
+using MagicVilla_CouponAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CouponRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task CreateAsync(Coupon coupon)
+        {
+            _db.Add(coupon);
+        }
+
+        public async Task<ICollection<Coupon>> GetAllAsync()
+        {
+            return await _db.Coupons.ToListAsync();
+        }
+
+        public async Task<Coupon> GetAsync(int id)
+        {
+            return await _db.Coupons.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<Coupon> GetAsync(string couponName)
+        {
+            return await _db.Coupons.FirstOrDefaultAsync(u => u.Name.ToLower() == couponName.ToLower());
+        }
+
+        public async Task RemoveAsync(Coupon coupon)
+        {
+            _db.Coupons.Remove(coupon);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Coupon coupon)
+        {
+            _db.Coupons.Update(coupon);
+        }
+    }
+}
---
+++ MagicVilla_CouponAPI/Repository/IRepository/ICouponRepository.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_CouponAPI.Models;
+
+namespace MagicVilla_CouponAPI.Repository.IRepository
+{
+    public interface ICouponRepository
+    {
+        Task<ICollection<Coupon>> GetAllAsync();
+        Task<Coupon> GetAsync(int id);
+        Task<Coupon> GetAsync(string couponName);
+        Task CreateAsync(Coupon coupon);
+        Task UpdateAsync(Coupon coupon);
+        Task RemoveAsync(Coupon coupon);
+        Task SaveAsync();
+    }
+}
